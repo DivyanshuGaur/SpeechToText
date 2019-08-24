e1efa2ad136e6f835ad5ca6be47d0c8438b0fecd
DivyanshuGaur <48865853+DivyanshuGaur@users.noreply.github.com>
2019-08-25 00:16:11 +0530

    Gui update

--- app/src/main/java/com/example/asus/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/asus/speechtotext/MainActivity.java
@@ -2,28 +2,33 @@ package com.example.asus.speechtotext;
 
 import android.content.Intent;
 import android.speech.RecognizerIntent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    FloatingActionButton fb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.textView3);
+        fb=(FloatingActionButton)findViewById(R.id.floatingActionButton4);
         Toast.makeText(this, "say!", Toast.LENGTH_SHORT).show();
     }
     public void getText(View v){
         Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
+        fb.setAlpha(0.5f);
          if(intent.resolveActivity(getPackageManager())!=null)
          startActivityForResult(intent,10);
         else
@@ -45,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
                  ArrayList <String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     Log.i("info",result.get(0));
                     Toast.makeText(this, result.get(0), Toast.LENGTH_SHORT).show();
-
+                    tv.setText(result.get(0));
+                    fb.setAlpha(1.0f);
 
                 }
                 break;
